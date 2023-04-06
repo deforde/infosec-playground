8aa9a811c71cb215ee255b3227ef0f4c21b08488
deforde <daniel.forde001@gmail.com>
2023-04-06 15:04:20 +0200

    update demo

--- stack-smashing/001/main2.c
+++ stack-smashing/001/main2.c
@@ -1,17 +1,25 @@
 int main(void) {
     // To detemine what opcodes are required in order to
-    // implement a sum function, compile the inline asm
-    // below, and inspect the generated opcodes by running:
-    // $ gdb test -ex="disassemble /r main"
+    // implement a sum function, compile the inline asm:
     //
     // asm volatile (
     //     "mov %edi,%eax\n\t"
     //     "add %esi,%eax\n\t"
     //     "ret"
     // );
+    //
+    // And inspect the generated opcodes by running:
+    // $ gdb test -ex="disassemble /r main"
+    // %esi and %edi are used to store function arguments,
+    // %eax stores the return value.
+    //
+    // These conventions change depending on machine & compiler,
+    // they can be tested by defining an equivalent (real) function
+    // and inspecting the generated asm.
+
     char sum[] = {
         0x89,0xf8,  // mov %edi,%eax
-        0x01,0xf0,  // add %eax,%esi
+        0x01,0xf0,  // add %esi,%eax
         0xc3        // ret
     };
     int (*sum_func)(int, int) = sum;
