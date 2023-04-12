b423935e7662e9085bcf88b57ee7d02c08b420d2
deforde <daniel.forde001@gmail.com>
2023-04-12 11:59:15 +0200

    pass all cmd line args to the sub-process in aslrd

--- tools/aslrd/aslrd.c
+++ tools/aslrd/aslrd.c
@@ -1,12 +1,16 @@
+#include <string.h>
+
 #include <unistd.h>
 #include <sys/syscall.h>
 #include <sys/personality.h>
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
+    if (argc < 2) {
         return 1;
     }
     syscall(SYS_personality, ADDR_NO_RANDOMIZE);
-    execve(argv[1], (char*[]){argv[1], NULL}, NULL);
+    memmove(argv, &argv[1], (argc - 1) * sizeof(*argv));
+    argv[argc - 1] = NULL;
+    execve(argv[0], argv, NULL);
     return 0;
 }
